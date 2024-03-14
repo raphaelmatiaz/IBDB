de35fe8a2cc6ec092ea9e00c22e77411f5af624f
Jessiellen <jessiellen@hotmail.com>
2024-03-14 20:31:34 +0000

    main page start

---
+++ Site front-end/slide.js
@@ -0,0 +1,12 @@
+// var radio = document.querySelector('.manual-btn')
+
+// document.getElementById('radio1').checked = true
+
+// setInterval(() => {
+//     nextImg()
+// },5000)
+
+// function nextImg() {
+//     cont++
+// }
+
